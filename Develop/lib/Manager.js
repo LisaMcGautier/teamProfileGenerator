5d861d579713eb57fe26a9161b4768b55cbff91a
Lisa McDonald <nixxxxer@aol.com>
2020-08-30 22:48:22 -0400

    Pass Manager Tests

--- Develop/lib/Manager.js
+++ Develop/lib/Manager.js
@@ -1,1 +1,34 @@
 // TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
+
+class Manager {
+    // Just like constructor functions, classes can accept arguments
+    constructor(name, id, email, officeNumber) {
+          this.name = name;
+          this.id = id;
+          this.email = email;
+          this.officeNumber = officeNumber;
+
+    }
+
+    getName() {
+        return this.name;
+    }
+
+    getId() {
+        return this.id;
+    }
+
+    getEmail() {
+        return this.email;
+    }
+
+    getRole() {
+        return "Manager";
+    }
+
+    getOfficeNumber() {
+        return this.officeNumber;
+    }
+}
+
+module.exports = Manager;
