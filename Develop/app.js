deb3e396f6431c4e8e8b9dbf2b83773f9cfe722c
Lisa McDonald <nixxxxer@aol.com>
2020-09-03 18:46:22 -0400

    Fixed Inquirer Prompt Format All Employee Types

--- Develop/app.js
+++ Develop/app.js
@@ -61,22 +61,26 @@ const engineerQuestions = [
     {
         type: "input",
         name: "name",
-        message: "What is your engineer's name?"
+        message: "What is your engineer's name?",
+        default: "David"
     },
     {
         type: "input",
         name: "id",
-        message: "What is your engineer's id?"
+        message: "What is your engineer's id?",
+        default: "56789"
     },
     {
         type: "input",
         name: "email",
-        message: "What is your engineer's email?"
+        message: "What is your engineer's email?",
+        default: "david@david.com"
     },
     {
         type: "input",
         name: "github",
-        message: "What is your engineer's GitHub username?"
+        message: "What is your engineer's GitHub username?",
+        default: "githubTAdavid"
     },
     {
         type: "list",
@@ -94,22 +98,26 @@ const internQuestions = [
     {
         type: "input",
         name: "name",
-        message: "What is your intern's name?"
+        message: "What is your intern's name?",
+        default: "Lisa"
     },
     {
         type: "input",
         name: "id",
-        message: "What is your intern's id?"
+        message: "What is your intern's id?",
+        default: "2020"
     },
     {
         type: "input",
         name: "email",
-        message: "What is your intern's email?"
+        message: "What is your intern's email?",
+        default: "lisa@lisa.com"
     },
     {
         type: "input",
         name: "school",
-        message: "What is your intern's school?"
+        message: "What is your intern's school?",
+        default: "UMiami"
     },
     {
         type: "list",
@@ -133,141 +141,152 @@ function appendToFile(fileName, readmeText) {
     });
 }
 
-async function promptUser(questions) {
-    return await inquirer.prompt(questions);
+// async function promptUser(questions) {
+//     return await inquirer.prompt(questions);
+// }
+
+// function promptManager(managerQuestions) {
+//     return inquirer.prompt(managerQuestions);
+// }
+
+// console.log("Manager: Please build your team.\n");
+
+// promptManager(managerQuestions)
+function promptManager(questions, employeeType) {
+    inquirer.prompt(questions)
+        .then(function (answers) {
+
+            let newTeamMember;
+
+            if (employeeType == "Manager") {
+                newTeamMember = new Manager(answers.name, answers.id, answers.email, answers.officeNumber);
+            } else if (employeeType == "Engineer") {
+                newTeamMember = new Engineer(answers.name, answers.id, answers.email, answers.github);
+            } else if (employeeType == "Intern") {
+                newTeamMember = new Intern(answers.name, answers.id, answers.email, answers.school);
+            }
+
+            // let manager = new Manager(answers.name, answers.id, answers.email, answers.officeNumber);
+            // let manager = new Manager(managerAnswers.name, managerAnswers.id, managerAnswers.email, managerAnswers.officeNumber);
+
+            teamMembers.push(newTeamMember);
+
+            // appendToFile("view.txt", renderManager(manager));
+            writeFileAsync("index.html", render.render(teamMembers));
+
+            // renderManager(manager);
+
+            if (answers.teamMember == "I don't want to add any more team members.") {
+                return answers;
+            } else if (answers.teamMember == "Engineer") {
+                // promptEngineer(engineerQuestions);
+                promptManager(engineerQuestions, "Engineer");
+            } else if (answers.teamMember == "Intern") {
+                // promptIntern(internQuestions);
+                promptManager(internQuestions, "Intern");
+            } else {
+                console.log("Error");
+            }
+
+            return answers;
+
+            // const html = generateHTML(answers);
+            // return writeFileAsync("index.html", html);
+
+        })
+
 }
-
-// const replacePlaceholders = (template, placeholder, value) => {
-//     const pattern = new RegExp("{{ " + placeholder + " }}", "gm");
-//     return template.replace(pattern, value);
-// };
-
-// const renderManager = manager => {
-//     let template = fs.readFileSync(path.resolve(templatesDir, "manager.html"), "utf8");
-//     template = replacePlaceholders(template, "name", manager.getName());
-//     template = replacePlaceholders(template, "role", manager.getRole());
-//     template = replacePlaceholders(template, "email", manager.getEmail());
-//     template = replacePlaceholders(template, "id", manager.getId());
-//     template = replacePlaceholders(template, "officeNumber", manager.getOfficeNumber());
-//     return template;
-// };
 
 console.log("Manager: Please build your team.\n");
 
-promptUser(managerQuestions)
-    .then(async function (answers) {
-
-        let manager = new Manager(answers.name, answers.id, answers.email, answers.officeNumber);
-        // console.log(manager.getName());
-
-        teamMembers.push(manager);
-        // console.log(JSON.stringify(answers));
-
-        // appendToFile("view.txt", renderManager(manager));
-        writeFileAsync("index.html", render.render(teamMembers));
-
-        // renderManager(manager);
-
-        if (answers.teamMember == "I don't want to add any more team members.") {
-            return answers;
-            // } else if (answers.teamMember == "Engineer") {
-            //     await promptUser(engineerQuestions);
-            // } else if (answers.teamMember == "Intern") {
-            //     await promptUser(internQuestions);
-            // } else {
-            //     console.log("Manager questions");
-        }
-
-        // console.log("Don't forget to write to HTML");
-
-        return await answers;
-
-        // const html = generateHTML(answers);
-        // return writeFileAsync("index.html", html);
-
-    })
-
-    // .then
-    //promptUser(engineerQuestions)
-    //     .then(async function (answers) {
-
-    //         let engineer = new Engineer(answers.name, answers.id, answers.email, answers.github);
-    //         console.log(engineer.getName());
-
-    //         teamMembers.push(engineer);
-    //         console.log(JSON.stringify(answers));
-
-    //         appendToFile("view.txt", JSON.stringify(answers));
-
-    //         if (answers.teamMember == "I don't want to add any more team members.") {
-    //             return answers;
-    //         } else if (answers.teamMember == "Engineer") {
-    //             await promptUser(engineerQuestions);
-    //         } else if (answers.teamMember == "Intern") {
-    //             await promptUser(internQuestions);
-    //         } else {
-    //             console.log("Engineer questions");
-    //         }
-
-    //         console.log("Don't forget to write to HTML");
-
-    //         return answers;
-
-    //         // const html = generateHTML(answers);
-    //         // return writeFileAsync("index.html", html);
-    //     })
-
-    // .then
-    //promptUser(internQuestions)
-    //     .then(async function (answers) {
-
-    //         let intern = new Intern(answers.name, answers.id, answers.email, answers.school);
-    //         console.log(intern.getName());
-
-    //         teamMembers.push(intern);
-    //         console.log(JSON.stringify(answers));
-
-    //         appendToFile("view.txt", JSON.stringify(answers));
-
-    //         if (answers.teamMember == "I don't want to add any more team members.") {
-    //             return answers;
-    //         } else if (answers.teamMember == "Engineer") {
-    //             await promptUser(engineerQuestions);
-    //         } else if (answers.teamMember == "Intern") {
-    //             await promptUser(internQuestions);
-    //         } else {
-    //             console.log("Intern questions");
-    //         }
-
-    //         console.log("Don't forget to write to HTML");
-
-    //         return answers;
-
-    //         // const html = generateHTML(answers);
-    //         // return writeFileAsync("index.html", html);
-    //     })
+promptManager(managerQuestions, "Manager");
 
 
-    .then(async function (answers) {
+
+// await 
+// .then
+//promptUser(engineerQuestions)
+// function promptEngineer(engineerQuestions) {
+//     return inquirer.prompt(engineerQuestions);
+// }
+
+// promptEngineer(engineerQuestions)
+//     .then(async function (engineerAnswers) {
+
+//         // let engineer = new Engineer(answers.name, answers.id, answers.email, answers.github);
+//         let engineer = new Engineer(engineerAnswers.name, engineerAnswers.id, engineerAnswers.email, engineerAnswers.github);
+//         // console.log(engineer.getName());
+
+//         teamMembers.push(engineer);
+//         // console.log(JSON.stringify(answers));
+
+//         // appendToFile("view.txt", renderManager(manager));
+//         await writeFileAsync("index.html", render.render(teamMembers));
+
+//         // renderEngineer(engineer);
+
+//         if (answers.teamMember == "I don't want to add any more team members.") {
+//             return answers;
+//         } else if (answers.teamMember == "Engineer") {
+//         promptEngineer(engineerQuestions);
+//         } else if (answers.teamMember == "Intern") {
+//         promptIntern(internQuestions);
+//         } else {
+//             console.log("Error");
+//         }
+
+//         return await answers;
+
+//         // const html = generateHTML(answers);
+//         // return writeFileAsync("index.html", html);
+//     })
+
+// // await 
+// // .then
+// //promptUser(internQuestions)
+// function promptIntern(internQuestions) {
+//     return inquirer.prompt(internQuestions);
+// }
+
+// promptIntern(internQuestions)
+//     .then(async function (internAnswers) {
+
+//         // let intern = new Intern(answers.name, answers.id, answers.email, answers.school);
+//         let intern = new Intern(internAnswers.name, internAnswers.id, internAnswers.email, internAnswers.school);
+//         // console.log(intern.getName());
+
+//         teamMembers.push(intern);
+//         // console.log(JSON.stringify(answers));
+
+//         // appendToFile("view.txt", JSON.stringify(answers));
+//         await writeFileAsync("index.html", render.render(teamMembers));
+
+//         if (answers.teamMember == "I don't want to add any more team members.") {
+//             return answers;
+//         } else if (answers.teamMember == "Engineer") {
+//             promptEngineer(engineerQuestions);
+//         } else if (answers.teamMember == "Intern") {
+//             promptIntern(internQuestions);
+//         } else {
+//             console.log("Error");
+//         }
+
+//         // console.log("Don't forget to write to HTML");
+
+//         return await answers;
+
+//         // const html = generateHTML(answers);
+//         // return writeFileAsync("index.html", html);
+//      })
 
 
-        // render(teamMembers);
+// .then(async function (answers) {
+//     // render(teamMembers);
+// })
 
-        
-        // if (answers.teamMember == "I don't want to add any more team members.") {
-        //     break;
-        // } else if (answers.teamMember == "Engineer") {
-        //     await promptUser(engineerQuestions);
-        // } else if (answers.teamMember == "Intern") {
-        //     await promptUser(internQuestions);
-        // } else {
-        //     console.log("Something else");
-        // }
-        // console.log("Don't forget to write to HTML");
-    })
-    .catch(function (err) {
-        console.log(err);
-    });
+// .catch(function (err) {
+//     console.log(err);
+// });
 
 
 
@@ -295,3 +314,31 @@ promptUser(managerQuestions)
 // see the directions for further information. 
 // Be sure to test out each class and verify it generates an object with the correct structure and methods. 
 // This structure will be crucial in order for the provided `render` function to work!
+
+
+
+//     // if (answers.teamMember == "I don't want to add any more team members.") {
+//     //     break;
+//     // } else if (answers.teamMember == "Engineer") {
+//     //     await promptUser(engineerQuestions);
+//     // } else if (answers.teamMember == "Intern") {
+//     //     await promptUser(internQuestions);
+//     // } else {
+//     //     console.log("Something else");
+//     // }
+//     // console.log("Don't forget to write to HTML");
+
+// const replacePlaceholders = (template, placeholder, value) => {
+//     const pattern = new RegExp("{{ " + placeholder + " }}", "gm");
+//     return template.replace(pattern, value);
+// };
+
+// const renderManager = manager => {
+//     let template = fs.readFileSync(path.resolve(templatesDir, "manager.html"), "utf8");
+//     template = replacePlaceholders(template, "name", manager.getName());
+//     template = replacePlaceholders(template, "role", manager.getRole());
+//     template = replacePlaceholders(template, "email", manager.getEmail());
+//     template = replacePlaceholders(template, "id", manager.getId());
+//     template = replacePlaceholders(template, "officeNumber", manager.getOfficeNumber());
+//     return template;
+// };
