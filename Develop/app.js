b1b4701f128f06904be846bcaa14a0843893fb03
Lisa McDonald <nixxxxer@aol.com>
2020-09-04 15:45:49 -0400

    Added Colors and Shadow

--- Develop/app.js
+++ Develop/app.js
@@ -134,27 +134,7 @@ const internQuestions = [
     },
 ];
 
-// function appendToFile(fileName, readmeText) {
-//     fs.appendFile(fileName, readmeText, function (error) {
-//         if (error) {
-//             console.log("Error: ", error);
-//         } else {
-//             console.log("view.txt generated!");
-//         }
-//     });
-// }
 
-// async function promptUser(questions) {
-//     return await inquirer.prompt(questions);
-// }
-
-// function promptManager(managerQuestions) {
-//     return inquirer.prompt(managerQuestions);
-// }
-
-// console.log("Manager: Please build your team.\n");
-
-// promptManager(managerQuestions)
 function promptManager(questions, employeeType) {
     inquirer.prompt(questions)
         .then(function (answers) {
@@ -169,32 +149,21 @@ function promptManager(questions, employeeType) {
                 newTeamMember = new Intern(answers.name, answers.id, answers.email, answers.school);
             }
 
-            // let manager = new Manager(answers.name, answers.id, answers.email, answers.officeNumber);
-            // let manager = new Manager(managerAnswers.name, managerAnswers.id, managerAnswers.email, managerAnswers.officeNumber);
-
             teamMembers.push(newTeamMember);
 
-            // appendToFile("view.txt", renderManager(manager));
             writeFileAsync("./output/team.html", render.render(teamMembers));
-
-            // renderManager(manager);
 
             if (answers.teamMember == "I don't want to add any more team members.") {
                 return answers;
             } else if (answers.teamMember == "Engineer") {
-                // promptEngineer(engineerQuestions);
                 promptManager(engineerQuestions, "Engineer");
             } else if (answers.teamMember == "Intern") {
-                // promptIntern(internQuestions);
                 promptManager(internQuestions, "Intern");
             } else {
                 console.log("Error");
             }
 
             return answers;
-
-            // const html = generateHTML(answers);
-            // return writeFileAsync("index.html", html);
 
         }).catch(function (err) {
             console.log(err);
@@ -227,125 +196,3 @@ promptManager(managerQuestions, "Manager");
 // Be sure to test out each class and verify it generates an object with the correct structure and methods. 
 // This structure will be crucial in order for the provided `render` function to work!
 
-
-
-// await 
-// .then
-//promptUser(engineerQuestions)
-// function promptEngineer(engineerQuestions) {
-//     return inquirer.prompt(engineerQuestions);
-// }
-
-// promptEngineer(engineerQuestions)
-//     .then(async function (engineerAnswers) {
-
-//         // let engineer = new Engineer(answers.name, answers.id, answers.email, answers.github);
-//         let engineer = new Engineer(engineerAnswers.name, engineerAnswers.id, engineerAnswers.email, engineerAnswers.github);
-//         // console.log(engineer.getName());
-
-//         teamMembers.push(engineer);
-//         // console.log(JSON.stringify(answers));
-
-//         // appendToFile("view.txt", renderManager(manager));
-//         await writeFileAsync("index.html", render.render(teamMembers));
-
-//         // renderEngineer(engineer);
-
-//         if (answers.teamMember == "I don't want to add any more team members.") {
-//             return answers;
-//         } else if (answers.teamMember == "Engineer") {
-//         promptEngineer(engineerQuestions);
-//         } else if (answers.teamMember == "Intern") {
-//         promptIntern(internQuestions);
-//         } else {
-//             console.log("Error");
-//         }
-
-//         return await answers;
-
-//         // const html = generateHTML(answers);
-//         // return writeFileAsync("index.html", html);
-//     })
-
-// // await 
-// // .then
-// //promptUser(internQuestions)
-// function promptIntern(internQuestions) {
-//     return inquirer.prompt(internQuestions);
-// }
-
-// promptIntern(internQuestions)
-//     .then(async function (internAnswers) {
-
-//         // let intern = new Intern(answers.name, answers.id, answers.email, answers.school);
-//         let intern = new Intern(internAnswers.name, internAnswers.id, internAnswers.email, internAnswers.school);
-//         // console.log(intern.getName());
-
-//         teamMembers.push(intern);
-//         // console.log(JSON.stringify(answers));
-
-//         // appendToFile("view.txt", JSON.stringify(answers));
-//         await writeFileAsync("index.html", render.render(teamMembers));
-
-//         if (answers.teamMember == "I don't want to add any more team members.") {
-//             return answers;
-//         } else if (answers.teamMember == "Engineer") {
-//             promptEngineer(engineerQuestions);
-//         } else if (answers.teamMember == "Intern") {
-//             promptIntern(internQuestions);
-//         } else {
-//             console.log("Error");
-//         }
-
-//         // console.log("Don't forget to write to HTML");
-
-//         return await answers;
-
-//         // const html = generateHTML(answers);
-//         // return writeFileAsync("index.html", html);
-//      })
-
-
-// .then(async function (answers) {
-//     // render(teamMembers);
-// })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//     // if (answers.teamMember == "I don't want to add any more team members.") {
-//     //     break;
-//     // } else if (answers.teamMember == "Engineer") {
-//     //     await promptUser(engineerQuestions);
-//     // } else if (answers.teamMember == "Intern") {
-//     //     await promptUser(internQuestions);
-//     // } else {
-//     //     console.log("Something else");
-//     // }
-//     // console.log("Don't forget to write to HTML");
-
-// const replacePlaceholders = (template, placeholder, value) => {
-//     const pattern = new RegExp("{{ " + placeholder + " }}", "gm");
-//     return template.replace(pattern, value);
-// };
-
-// const renderManager = manager => {
-//     let template = fs.readFileSync(path.resolve(templatesDir, "manager.html"), "utf8");
-//     template = replacePlaceholders(template, "name", manager.getName());
-//     template = replacePlaceholders(template, "role", manager.getRole());
-//     template = replacePlaceholders(template, "email", manager.getEmail());
-//     template = replacePlaceholders(template, "id", manager.getId());
-//     template = replacePlaceholders(template, "officeNumber", manager.getOfficeNumber());
-//     return template;
-// };
